bed2cf58df9ac0df1e2982fc7ff9debddb899ec7
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-09-24 21:33:56 +0100

    100

---
+++ 100-shell_sort.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+
+/**
+ * shell_sort - sorts array in ascending order using shell sort algorithm
+ * @array: array to sort
+ * @size: size of the array
+ *
+ * Return: void
+ */
+void shell_sort(int *array, size_t size)
+{
+	size_t gap = 0, i, j;
+	int tmp;
+
+	while (gap < size / 3)
+		gap = gap * 3 + 1;
+	for (; gap >= 1; gap = (gap - 1) / 3)
+	{
+		for (j = gap; j < size; j++)
+		{
+			for (i = j - gap; (int)i >= 0; i--)
+			{
+				if (array[i + gap] < array[i])
+				{
+					tmp = array[i];
+					array[i] = array[i + gap];
+					array[i + gap] = tmp;
+					print_array((const int*)array, size);
+				}
+				else
+					break;
+			}
+		}
+	}
+}
