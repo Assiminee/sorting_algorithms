1bc7924dad4d4378cc26d3af073be89fbeab0678
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-09-24 22:13:47 +0100

    100

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -9,7 +9,7 @@
  */
 void shell_sort(int *array, size_t size)
 {
-	size_t gap = 0, i, j;
+	size_t gap = 1, i, j;
 	int tmp;
 
 	if (array == NULL || size < 2)
@@ -18,7 +18,7 @@ void shell_sort(int *array, size_t size)
 	while (gap < size / 3)
 		gap = gap * 3 + 1;
 
-	for (; gap >= 1; gap = gap / 3)
+	for (; gap >= 1; gap /= 3)
 	{
 		for (i = gap; i < size; i++)
 		{
